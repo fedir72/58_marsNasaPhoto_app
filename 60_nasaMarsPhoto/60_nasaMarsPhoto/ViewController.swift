9d10499054504cb7e6c77ebcc181dbabc09c0088
fedir72 <fedir72@ua.fm>
2020-09-02 14:17:16 +0300

    start project

---
+++ 60_nasaMarsPhoto/60_nasaMarsPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  60_nasaMarsPhoto
+//
+//  Created by fedir on 02.09.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
